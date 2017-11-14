843a1ec1f5c8a5c3fa1d4aa1d41fdf3b47275e44
Atharva Dandekar <dandekar.atharva@gmail.com>
2017-11-14 00:30:57 -0500

    Added block download overlay.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,4 +12,14 @@ angular.module('dbfsWebappApp').controller('MainCtrl', function ($scope) {
     $scope.totalFileUploads = 20;
     $scope.totalNodes = 3;
     $scope.counterDuration = 1;
+
+    $('.node-block').on('mouseover', function() {
+        // $(this).find('table').addClass('active-defocus');
+        $(this).find('.node-block-defocus-panel').show();
+    });
+
+    $('.node-block').on('mouseout', function() {
+        // $(this).find('table').removeClass('active-defocus');
+        $(this).find('.node-block-defocus-panel').hide();
+    });
 });
