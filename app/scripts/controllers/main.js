c2cd682bc8bd0edeb0a0a6c3ccba08cde740907c
Atharva Dandekar <dandekar.atharva@gmail.com>
2017-11-17 11:05:17 -0500

    Added local search using angular filter.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,7 +12,7 @@ angular.module('dbfsWebappApp').controller('MainCtrl', function ($scope) {
     $scope.totalFileUploads = 20;
     $scope.totalNodes = 3;
     $scope.counterDuration = 1;
-    $scope.isKeyPresent = false;
+    $scope.isKeyPresent = false;    
 
     $scope.blocklist = [
         {
@@ -20,11 +20,11 @@ angular.module('dbfsWebappApp').controller('MainCtrl', function ($scope) {
             hash: '0xfe88c94d860f01a17f961bf4bdfb6e0c6cd10d3fda5cc861e805ca1240c58553',
             timestamp: '321232132423'
         },{
-            filename: 'sample.mp4',
+            filename: 'myfile.mp4',
             hash: '0xfe88c94d860f01a17f961bf4bdfb6e0c6cd10d3fda5cc861e805ca1240c58553',
             timestamp: '321232132423'
         },{
-            filename: 'sample.mp4',
+            filename: 'Blizzard.mp4',
             hash: '0xfe88c94d860f01a17f961bf4bdfb6e0c6cd10d3fda5cc861e805ca1240c58553',
             timestamp: '321232132423'
         }
@@ -38,7 +38,7 @@ angular.module('dbfsWebappApp').controller('MainCtrl', function ($scope) {
             $(this).find('table').removeClass('active-defocus');
             $(this).find('.node-block-defocus-panel').hide();
         });
-    });    
+    });
 
     // $(document).on('mouseover', '.node-block', function() {
     //     $(this).find('table').addClass('active-defocus');
