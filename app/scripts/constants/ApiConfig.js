6ffd94b701a3cf2bfd9d4f7474a70a672112fe79
Atharva Dandekar <dandekar.atharva@gmail.com>
2017-12-01 10:35:48 -0500

    Added constants folder and apiconfig file to define constants.

---
+++ app/scripts/constants/ApiConfig.js
@@ -0,0 +1,9 @@
+"use strict";
+
+angular.module('dbfsWebappApp').factory('ApiConfig', function() {
+    var API_URL = 'http://localhost/someapi';
+
+    return {
+        API_URL: API_URL
+    };
+});
